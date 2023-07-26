4fdddb0258a41b998752d5165444413c9f8fa0e6
ZahraNematzadeh <zahra.nematzadeh87@gmail.com>
2023-07-26 17:42:37 +0000

    Modify codes

--- src/make_dataset_folder.py
+++ src/make_dataset_folder.py
@@ -1,8 +1,8 @@
 import os 
-def make_dataset_folder(folder_path, dataset_folder):
 
-	if not os.path.exists(folder_path):
-   		 os.makedirs(folder_path)
-	dataset_folder = os.path.join(folder_path, dataset_name)
-	if not os.path.exists(dataset_folder):
-   		 os.makedirs(dataset_folder)
+def make_dataset_folder(folder_path, dataset_name):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    dataset_folder = os.path.join(folder_path, dataset_name)
+    if not os.path.exists(dataset_folder):
+        os.makedirs(dataset_folder)
