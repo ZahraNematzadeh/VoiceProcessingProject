ed5a1203e78a67e6892141ff12a372bdc7d996fa
ZahraNematzadeh <zahra.nematzadeh87@gmail.com>
2023-07-27 10:43:16 -0400

    Update creating big mass

--- helper/creating_big_mass.py
+++ helper/creating_big_mass.py
@@ -7,9 +7,12 @@ import os
 import shutil
 import pandas as pd
 
-data = pd.read_csv('C:/Users/zahra/Desktop/FarzanehFiles/Codes/data.csv', usecols=['MRN', 'Diagnosis', 'Type_of_Mass'])
-positive_folder = 'C:/Users/zahra/Desktop/FarzanehFiles/Codes/2_Code_TT_AugPercentAllTrain_TV/OnlineAugmentation/OutputOnline/1.e/2_DataLabeling/CleanedData/Positive'
-output_folder = 'C:/Users/zahra/Desktop/FarzanehFiles/Codes/2_Code_TT_AugPercentAllTrain_TV/OnlineAugmentation/OutputOnline/1.e/2_DataLabeling/CleanedData/big_mass/Positive'
+data = pd.read_csv('C:/Users/zahra/VoiceColab - Copy/dataset/metadata/data.csv', usecols=['MRN', 'Diagnosis', 'Type_of_Mass'])
+positive_folder = 'C:/Users/zahra/Desktop/FarzanehFiles/DataFolders/Wav_format/SplittedData/1_e/Positive'
+output_folder = 'C:/Users/zahra/VoiceColab - Copy/dataset/e/test_train/ClusteredData/big_mass_wav/Positive'
+
+if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
 big_mass = data[(data['Diagnosis'] == 'Mass') & (data['Type_of_Mass'].isin(['Papilloma', 'Cancer', 'Polyp']))].copy()
 big_mass.dropna(inplace=True)
@@ -20,16 +23,15 @@ print("Number of big_mass in csv file:", big_mass_count)
 
 #%% Copy the Big mass to the desired folder
 counter = 0
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
 mrns = big_mass['MRN'].tolist()
-
 file_dict = {}
 for filename in os.listdir(positive_folder):
     file_path = os.path.join(positive_folder, filename)
     if os.path.isfile(file_path):
         mrn = filename.split(".")[0]
-        if mrn in mrns:
+        first_dash_index = mrn.find('-')
+        desired_substring = mrn[first_dash_index + 1:]
+        if desired_substring in mrns:
             if mrn not in file_dict:
                 file_dict[mrn] = 1
             else:
@@ -41,15 +43,3 @@ for filename in os.listdir(positive_folder):
             counter += 1
 print(f"Total samples copied: {counter}")
 
-#%% 
-'''
-def count_items_in_folder(folder_path):
-    count = 0
-    for _, _, files in os.walk(folder_path):
-        count += len(files)
-    return count
-
-folder_path = "C:/Users/zahra/Desktop/FarzanehFiles/Codes/2_Code_TT_AugPercentAllTrain_TV/OnlineAugmentation/OutputOnline/1.e/2_DataLabeling/CleanedData/Positive" 
-item_count = count_items_in_folder(folder_path)
-print("Number of items in the folder:", item_count)
-'''
