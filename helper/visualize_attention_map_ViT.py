cc7790aee0b9414e5329eb53d0c4693c1862a483
ZahraNematzadeh <zahra.nematzadeh87@gmail.com>
2023-09-20 13:24:22 -0400

    Add Code.

--- helper/visualize_attention_map_ViT.py
+++ helper/visualize_attention_map_ViT.py
@@ -1,23 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from vit_keras import visualize
+from tensorflow.keras.models import load_model
 
-output_train = np.load('C:/Users/zahra/VoiceColab/outputs/HelpersOutputs/1_e/big_mass/Melspectrogram/ViT/output_train.npy')
+loaded_model = load_model('C:/Users/zahra/VoiceColab/outputs/FinalOutputs/dataset/big_mass/Melspectrogram/ViT/dataset_model_ViT.hdf5')
+output_train = np.load('C:/Users/zahra/VoiceColab/outputs/HelpersOutputs/dataset/big_mass/Melspectrogram/ViT/output_train.npy')
 
-image = output_train[0][0]
+image = output_train[0]
 
-attention_map = visualize.attention_map(model = model, image = image)
-attention_map = visualize.attention_map(model=model.layers[0], image=image)
-
-
-attention_map1 = visualize.attention_map(model = vit_model_t.get_layer('vit_model'), image = res)
+#attention_map = visualize.attention_map(model=loaded_model.layers[0], image=image)
+attention_map = visualize.attention_map(model = loaded_model.get_layer('vit-b16'), image = image)
 
 fig = plt.figure(figsize=(20,20))
 ax = plt.subplot(1, 2, 1)
 ax.axis('off')
-ax.set_title('Original')
-_ = ax.imshow(res)
+ax.set_title('Original', fontsize=32)
+_ = ax.imshow(image)
 
 ax = plt.subplot(1, 2, 2)
 ax.axis('off')
-ax.set_title('Attention Map')
-_ = ax.imshow(attention_map1)
+ax.set_title('Attention Map', fontsize=32)
+_ = ax.imshow(attention_map)
+
+plt.show()
+
+'''
+import librosa.display
+spec = np.mean(image, axis=-1)
+spec = np.squeeze(spec)
+librosa.display.specshow(spec)
+'''
